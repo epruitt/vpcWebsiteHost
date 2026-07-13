8352cd5258798be2625aa0a840dd9c419e0ec4e1
E Pruitt <epruitt223@gmail.com>
2026-07-12 19:14:24 -0500

    fix terraform error

--- vpcWebsiteHosting/modules/vpc/outputs.tf
+++ vpcWebsiteHosting/modules/vpc/outputs.tf
@@ -75,5 +75,5 @@ output "target_group_arn_suffix" {
 
 output "iam_instance_profile_name" {
   description = "The name of the IAM instance profile"
-  value       = aws_iam_instance_profile.ec2_profile.name
+  value = aws_iam_instance_profile.ec2_instance_profile.name
 }
